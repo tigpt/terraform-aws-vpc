5703ddab06c6fc0b4eac5ba0e1b891910f664318
Tiago Rodrigues <mail@tig.pt>
2024-02-14 11:50:40 +0000

    remove comments

--- examples/tgw-subnets/tgw.tf
+++ examples/tgw-subnets/tgw.tf
@@ -7,26 +7,6 @@ module "tgw" {
 
   enable_auto_accept_shared_attachments = true
 
-  # vpc_attachments = {
-  #   network_prod_vpc = {
-  #     vpc_id       = module.vpc.vpc_id
-  #     subnet_ids   = module.vpc.intra_subnets
-  #     dns_support  = true
-  #     ipv6_support = false
-
-  #     tgw_routes = [
-  #       {
-  #         blackhole              = false
-  #         destination_cidr_block = "0.0.0.0/0"
-  #       },
-  #       # {
-  #       #   blackhole = false
-  #       #   destination_cidr_block = "40.0.0.0/20"
-  #       # }
-  #     ]
-  #   }
-  # }
   ram_name                      = "network-tgw"
   ram_allow_external_principals = true
-  # ram_principals                = [local.account_id.network-prod, local.account_id.shared-infra-prod]
 }
